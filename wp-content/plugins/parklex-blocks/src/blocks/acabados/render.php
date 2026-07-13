7cea3fd011b1144976af763de43b16c90ecb5fee
eletitos <36730057+eletitos@users.noreply.github.com>
2026-07-13 12:30:00 +0200

    Imagen en el hover del bloque acabados

--- wp-content/plugins/parklex-blocks/src/blocks/acabados/render.php
+++ wp-content/plugins/parklex-blocks/src/blocks/acabados/render.php
@@ -37,10 +37,16 @@ if ( ! $query->have_posts() ) {
 			<?php
 			while ( $query->have_posts() ) :
 				$query->the_post();
+
+				$gallery         = function_exists( 'get_field' ) ? get_field( 'gallery', get_the_ID() ) : false;
+				$hover_image_id  = ! empty( $gallery ) ? (int) $gallery[0] : 0;
 				?>
 				<a class="b-acabados__item swiper-slide" href="<?php echo esc_url( get_permalink() ); ?>">
 					<div class="b-acabados__image">
 						<?php echo get_the_post_thumbnail( get_the_ID(), 'medium', array( 'class' => 'b-acabados__img' ) ); ?>
+						<?php if ( $hover_image_id ) : ?>
+							<?php echo wp_get_attachment_image( $hover_image_id, 'medium', false, array( 'class' => 'b-acabados__img b-acabados__img--hover' ) ); ?>
+						<?php endif; ?>
 					</div>
 					<h3 class="b-acabados__title has-display-m-font-size"><?php echo esc_html( get_the_title() ); ?></h3>
 				</a>
