ee0ac6a84984cde8595285de3cf85c209ebb9b03
eletitos <36730057+eletitos@users.noreply.github.com>
2026-07-15 14:05:37 +0200

    Funcionamiento scroll over

--- wp-content/plugins/parklex-blocks/src/blocks/project-scrollover/render.php
+++ wp-content/plugins/parklex-blocks/src/blocks/project-scrollover/render.php
@@ -9,21 +9,30 @@ $project_ids = array_filter( array_map( 'absint', $attributes['projects'] ?? [] 
 ?>
 
 <section <?php echo bis_get_block_prop( $block, true ); ?>>
-	<div class="b-project-scrollover__content">
+	<div class="b-project-scrollover__media-wrap alignfull">
 		<?php echo $content; ?>
 	</div>
-	<div class="b-project-scrollover__projects">
+	<div class="b-project-scrollover__projects alignwide">
 		<?php foreach ( $project_ids as $project_id ) : ?>
+			<?php if ( ! $project_id || 'publish' !== get_post_status( $project_id ) ) continue; ?>
+
 			<?php
-			$title = get_the_title( $project_id );
-			if ( ! $title ) {
-				continue;
-			}
+			$author_terms = get_the_terms( $project_id, 'project_author' );
+			$author_name  = ( ! empty( $author_terms ) && ! is_wp_error( $author_terms ) ) ? $author_terms[0]->name : '';
 			?>
+
 			<div class="b-project-scrollover__project">
-				<?php echo wp_get_attachment_image( get_post_thumbnail_id( $project_id ), 'large', false, [ 'class' => 'b-project-scrollover__image' ] ); ?>
-				<span class="b-project-scrollover__title"><?php echo esc_html( $title ); ?></span>
+				<a class="b-project-scrollover__image" href="<?php echo esc_url( get_permalink( $project_id ) ); ?>">
+					<?php echo get_the_post_thumbnail( $project_id, 'large', array( 'class' => 'b-project-scrollover__img' ) ); ?>
+				</a>
+				<p class="b-project-scrollover__caption has-caption-font-size">
+					<span class="b-project-scrollover__title"><?php echo esc_html( get_the_title( $project_id ) ); ?></span>
+					<?php if ( $author_name ) : ?>
+						<span class="b-project-scrollover__author"><?php echo esc_html( $author_name ); ?></span>
+					<?php endif; ?>
+				</p>
 			</div>
 		<?php endforeach; ?>
+		<div class="b-project-scrollover__spacer" style="height: 100vh; width: 100%;"></div>
 	</div>
 </section>
