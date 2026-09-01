c305a0886c5a32e4be5c976824ad2fd54b370cad
eletitos <eletitos@gmail.com>
2026-09-01 21:47:10 +0200

    Filtrados por parámetros

--- wp-content/themes/parklex/archive-technical-card.php
+++ wp-content/themes/parklex/archive-technical-card.php
@@ -7,6 +7,10 @@ get_header();
 <main class="entry-content is-layout-constrained">
 	<div class="c-technical-card-categories alignfull">
 		<?php
+		$active_category = isset( $_GET['category_technical_card'] )
+			? sanitize_title( wp_unslash( $_GET['category_technical_card'] ) )
+			: '';
+
 		$technical_card_categories = get_terms(
 			array(
 				'taxonomy'   => 'category_technical_card',
@@ -17,19 +21,80 @@ get_header();
 		<?php if ( ! empty( $technical_card_categories ) && ! is_wp_error( $technical_card_categories ) ) : ?>
 			<ul class="c-technical-card-categories__list alignwide">
 				<li class="c-technical-card-categories__item">
-					<a class="c-technical-card-categories__link" href="<?php echo esc_url( get_post_type_archive_link( 'technical-card' ) ); ?>">
+					<a
+						class="c-technical-card-categories__link<?php echo '' === $active_category ? ' is-active' : ''; ?>"
+						href="<?php echo esc_url( remove_query_arg( 'category_technical_card' ) ); ?>"
+					>
 						<?php esc_html_e( 'All', 'parklex' ); ?>
 					</a>
 				</li>
 				<?php foreach ( $technical_card_categories as $technical_card_category ) : ?>
 					<li class="c-technical-card-categories__item">
-						<a class="c-technical-card-categories__link" href="<?php echo esc_url( get_term_link( $technical_card_category ) ); ?>">
+						<a
+							class="c-technical-card-categories__link<?php echo $active_category === $technical_card_category->slug ? ' is-active' : ''; ?>"
+							href="<?php echo esc_url( add_query_arg( 'category_technical_card', $technical_card_category->slug ) ); ?>"
+						>
 							<?php echo esc_html( $technical_card_category->name ); ?>
 						</a>
 					</li>
 				<?php endforeach; ?>
 			</ul>
 		<?php endif; ?>
+	</div>
+	<div class="c-technical-card-body alignwide">
+		<div class="c-technical-card-body__sidebar">
+			<?php
+			$active_classification = isset( $_GET['classification_technical_card'] )
+				? sanitize_title( wp_unslash( $_GET['classification_technical_card'] ) )
+				: '';
+
+			$classification_parents = get_terms(
+				array(
+					'taxonomy'   => 'classification_technical_card',
+					'hide_empty' => false,
+					'parent'     => 0,
+				)
+			);
+			?>
+			<?php if ( ! empty( $classification_parents ) && ! is_wp_error( $classification_parents ) ) : ?>
+				<?php foreach ( $classification_parents as $classification_parent ) : ?>
+					<?php
+					$classification_children = get_terms(
+						array(
+							'taxonomy'   => 'classification_technical_card',
+							'hide_empty' => false,
+							'parent'     => $classification_parent->term_id,
+						)
+					);
+					?>
+					<div class="c-technical-card-body__sidebar-group">
+						<span class="c-technical-card-body__sidebar-parent"><?php echo esc_html( $classification_parent->name ); ?></span>
+						<?php if ( ! empty( $classification_children ) && ! is_wp_error( $classification_children ) ) : ?>
+							<ul class="c-technical-card-body__sidebar-children">
+								<?php foreach ( $classification_children as $classification_child ) : ?>
+									<li class="c-technical-card-body__sidebar-item">
+										<a
+											class="c-technical-card-body__sidebar-link<?php echo $active_classification === $classification_child->slug ? ' is-active' : ''; ?>"
+											href="<?php echo esc_url( add_query_arg( 'classification_technical_card', $classification_child->slug ) ); ?>"
+										>
+											<?php echo esc_html( $classification_child->name ); ?>
+										</a>
+									</li>
+								<?php endforeach; ?>
+							</ul>
+						<?php endif; ?>
+					</div>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</div>
+		<div class="c-technical-card-list__grid">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				the_content();
+			endwhile;
+			?>
+		</div>
 	</div>
 	<?php
 	while ( have_posts() ) :
--- wp-content/themes/parklex/header.php
+++ wp-content/themes/parklex/header.php
@@ -9,7 +9,7 @@ defined( 'ABSPATH' ) || exit;
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class( 'wp-site-blocks' ); ?>>
+<body <?php body_class( 'wp-site-blocks' ); ?> data-device="<?php echo wp_is_mobile() ? 'movil' : 'desktop'; ?>">
 <?php wp_body_open(); ?>
 
 <header>
